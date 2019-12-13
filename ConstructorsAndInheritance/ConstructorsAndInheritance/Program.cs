519ddc493888505e4521c9a6f958bffd773691d6
Aman Bhardwaj <amanbhardwaj.04091997@gmail.com>
2019-12-13 20:49:37 +0530

    Last Updated 12/13/2019

---
+++ ConstructorsAndInheritance/ConstructorsAndInheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConstructorsAndInheritance
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			var car = new Car("123");
+		}
+
+	}
+
+	public class Vehicle
+	{
+		private readonly string _registrationNumber;
+
+		public Vehicle(string registrationNumber)
+		{
+			this._registrationNumber = registrationNumber;
+			Console.WriteLine("Param of Vehicle");
+		}
+
+	}
+
+	public class Car : Vehicle
+	{
+		public Car(string regNum) : base(regNum)
+		{
+			Console.WriteLine("Car Default");
+
+		}
+		// without base(regNum) -> error as Car would have tried to call the default contructor of Vehicle, which doesn't exists
+	}
+
+
+}
