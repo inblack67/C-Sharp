40c0272a06e3a09e6586da454a501e964feff1b6
Aman Bhardwaj <amanbhardwaj.04091997@gmail.com>
2019-12-14 22:12:14 +0530

    Last Updated 12/14/2019

---
+++ DelegatesIntro/DelegatesIntro/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace DelegatesIntro
+{
+    public class Photo
+    {
+
+    }
+
+    public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Applying Brightness...");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Applying Contrast...");
+        }
+
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resizing...");
+        }
+    }
+
+    public class PhotoProcessor
+    {
+        public delegate void PhotoFilterHandler(Photo photo);
+
+        public void Process(string path,PhotoFilterHandler photoFilterHandler)
+        {
+            var photo = new Photo();
+            photoFilterHandler(photo);
+        }
+
+        // built in delegate Action<>
+
+        public void Process2(string path, Action<Photo> action)
+        {
+            var photo = new Photo();
+            action(photo);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var photo = new Photo();
+            var photofilters = new PhotoFilters();
+            var photoprocessor = new PhotoProcessor();
+
+            PhotoProcessor.PhotoFilterHandler filterHandler = photofilters.ApplyBrightness;
+
+            filterHandler += photofilters.ApplyContrast;
+
+            filterHandler += RemoveRedEye;  // even our custome method is added without redeploying anythang
+
+            photoprocessor.Process("path",filterHandler);
+
+            Console.WriteLine();
+
+            // inbuilt delegate -> Action
+            Console.WriteLine("Action The Custom one -> ");
+
+            Action<Photo> customDelegate = photofilters.ApplyBrightness;
+            customDelegate += photofilters.ApplyContrast;
+            customDelegate += RemoveRedEye;
+
+            photoprocessor.Process2("path",customDelegate);
+
+        }
+
+        static void RemoveRedEye(Photo photo)
+        {
+            Console.WriteLine("Removing the red eye");
+        }
+    }
+}
---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaExpressions
+{
+    public class Book
+    {
+        public float Price { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class BookRepo
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() { Title="Title 1", Price=5},
+                new Book() { Title="Title 2", Price=7},
+                new Book() { Title="Title 3", Price=17}
+            };
+        }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int factor = 10;
+
+            Func<int, int> squareWithLambda = n => n * n;
+            Console.WriteLine("Square -> " + squareWithLambda(25));
+
+            Func<int, int> fact = n => n * factor;
+            Console.WriteLine("Factor -> " + fact(10));
+
+            // write the code to return all the books cheaper than $10
+            Console.WriteLine();
+            var books = new BookRepo().GetBooks();
+
+            //var cheapBooks = books.FindAll(IsCheaperThan10Dollars);
+
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+        }
+
+        //static bool IsCheaperThan10Dollars(Book book)
+        //{
+        //    return book.Price < 10;
+        //}
+
+
+
+        //public int Square(int n)
+        //{
+        //    return n ^ n;
+        //}
+    }
+}
