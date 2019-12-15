5a8f5f3020d91806b075eca9d78065e666dfb934
Aman Bhardwaj <amanbhardwaj.04091997@gmail.com>
2019-12-15 21:55:03 +0530

    Last Updated 12/15/2019

--- Dynamic/Dynamic/Program.cs
+++ Dynamic/Dynamic/Program.cs
@@ -8,7 +8,35 @@ namespace Dynamic
         {
             object obj = "Alex Turner";
             obj.GetHashCode();
-            Console.WriteLine(obj.GetHashCode());
+
+            //REFLECTION
+            //var methodInfo = obj.GetType().GetMethod("GetHashCode()");
+            //methodInfo.Invoke(null,null);
+
+
+            dynamic excelObject = "AM";
+            excelObject.Optimize();
+            //object excelObject = "AM";        
+            //excelObject.Optimize();       // object class does'nt contain any Optimize() method
+
+            dynamic name = "Me";
+            //name++; // RuntimeBinderException
+            name = 1;   // Python is not the only python in the industry bitch
+
+            dynamic a = 10, b = 9;
+            var c = a + b; // c ended up being a dynamic too
+
+            int i = 5;
+            dynamic d = i;      // d has become an integer
+
+            long l = 10;
+            d = l; //When converting from dynamic to static types,
+            // if the runtime type of the dynamic object is implicitly convertible
+            // to the target type, casting is not needed.
+
+
+            
+
         }
     }
 }
