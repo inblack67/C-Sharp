1f81cfee7ed9c27cc8afb479459f55b07d9d4a8f
Aman Bhardwaj <amanbhardwaj.04091997@gmail.com>
2019-12-15 21:53:12 +0530

    Last Updated 12/15/2019

---
+++ Dynamic/Dynamic/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "Alex Turner";
+            obj.GetHashCode();
+            Console.WriteLine(obj.GetHashCode());
+        }
+    }
+}
